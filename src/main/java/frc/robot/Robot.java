d300305555d779fe635948b3280e4b8caf7ecb2d
Deven Speyrer <devenspeyrer@gmail.com>
2022-12-05 17:08:56 -0700

    revert

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,22 +2,23 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
 import frc.robot.cmd.Axis;
 import frc.robot.cmd.Transit;
-
 import frc.robot.subsystems.Motor;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class Robot extends TimedRobot {
+	private final Motor transitMotor = new Motor(0);
+	private final Motor axisMotor = new Motor(1);
+
 	private final Joystick joystick = new Joystick(0);
 
 	@Override
 	public void robotInit() {
-		new Axis(new Motor(1), () -> joystick.getRawAxis(0));
-		new JoystickButton(joystick, 1).whenPressed(new Transit(new Motor(0)));
+		new Axis(axisMotor, () -> joystick.getRawAxis(0));
+		new JoystickButton(joystick, 1).whenPressed(new Transit(transitMotor));
 	}
 
 	@Override
